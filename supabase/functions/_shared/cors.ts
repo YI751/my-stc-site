0dd8027747413bc1e2dbea0fd406df489a2958b3
YI751 <yuma.ito@gendai-a.co.jp>
2025-10-27 18:09:26 +0900

    Delete supabase/functions directory

--- supabase/functions/_shared/cors.ts
+++
@@ -1,5 +0,0 @@
-// Supabase Edge Functions で共通して使用するCORSヘッダー
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-};
